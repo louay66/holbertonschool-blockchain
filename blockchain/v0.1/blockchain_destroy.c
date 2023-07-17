e7cd6b79bebb23bd3ebe19d7870d47d5654b62f6
louay66 <louaylouay20@gmail.com>
2023-07-17 15:27:49 +0100

    blockchain_destroy.c

---
+++ blockchain/v0.1/blockchain_destroy.c
@@ -0,0 +1,14 @@
+#include "blockchain.h"
+/**
+ * blockchain_destroy-destroy a blockchai
+ * @blockchain: pointer of blockchain to free
+ * Return: void
+ */
+void blockchain_destroy(blockchain_t *blockchain)
+{
+	if (!blockchain)
+		return;
+	llist_destroy(blockchain->chain, 1, (void (*)(llist_node_t))free);
+	free(blockchain);
+}
+
