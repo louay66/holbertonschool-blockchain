1db2d84a3154564d9d4cc4dd8e02ee97d7c9e6b1
louay66 <louaylouay20@gmail.com>
2023-07-28 15:19:34 +0100

    block_is_valid.c

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -45,7 +45,5 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 		return (1);
 	if (!hash_matches_difficulty(block->hash, block->info.difficulty))
 		return (1);
-	if (!hash_matches_difficulty(prev_block->hash, prev_block->info.difficulty))
-		return (1);
 	return (0);
 }
