7e473f4d7d7911a8879f950a6b7f8eae7c416895
louay66 <louaylouay20@gmail.com>
2023-08-06 00:03:08 +0100

    blockchain_destroy.c

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -9,6 +9,6 @@ void blockchain_destroy(blockchain_t *blockchain)
 	if (!blockchain)
 		return;
 	llist_destroy(blockchain->chain, 1, (void (*)(llist_node_t))free);
+	llist_destroy(blockchain->unspent, 1, free);
 	free(blockchain);
 }
-
