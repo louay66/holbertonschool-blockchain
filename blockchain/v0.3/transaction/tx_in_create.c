b8a2960a98f3159938cc9061456dc602bf9d1b3b
louay66 <louaylouay20@gmail.com>
2023-08-08 22:20:09 +0100

    done

--- blockchain/v0.3/transaction/tx_in_create.c
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -2,7 +2,8 @@
 
 /**
  * tx_in_create- allocates and initializes a transaction input structure
- * @unspent:points to the unspent transaction output to be converted to a transaction input
+ * @unspent:points to the unspent transaction output
+ * to be converted to a transaction input
  * Return: if successful return a pointer to created  transaction input
  * upon success, or NULL upon failure
  */
@@ -11,7 +12,7 @@ tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
 	tx_in_t *tx_in;
 
 	if (!unspent)
-		return NULL;
+		return (NULL);
 
 	tx_in = calloc(1, sizeof(tx_in_t));
 	memcpy(tx_in->block_hash, unspent->block_hash, SHA256_DIGEST_LENGTH);
