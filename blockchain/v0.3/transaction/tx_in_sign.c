3d63ca5eaba725549d610e891184373a3c0810bc
louay66 <louaylouay20@gmail.com>
2023-08-10 22:46:32 +0100

    test

--- blockchain/v0.3/transaction/tx_in_sign.c
+++ blockchain/v0.3/transaction/tx_in_sign.c
@@ -16,7 +16,6 @@ sig_t *tx_in_sign(tx_in_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH],
 						EC_KEY const *sender, llist_t *all_unspent)
 {
 	uint8_t pub_of_sender[EC_PUB_LEN];
-	uint8_t pub_of_reciver[EC_PUB_LEN];
 	unspent_tx_out_t *unspent;
 	unsigned int i;
 
