4aa3627ef5e41b55c9990836eb9e648ba500292d
louay66 <louaylouay20@gmail.com>
2023-08-10 22:45:31 +0100

    test

--- blockchain/v0.3/transaction/tx_in_sign.c
+++ blockchain/v0.3/transaction/tx_in_sign.c
@@ -13,7 +13,7 @@
  */
 
 sig_t *tx_in_sign(tx_in_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH],
-		EC_KEY const *sender, llist_t *all_unspent)
+						EC_KEY const *sender, llist_t *all_unspent)
 {
 	uint8_t pub_of_sender[EC_PUB_LEN];
 	uint8_t pub_of_reciver[EC_PUB_LEN];
@@ -29,7 +29,6 @@ sig_t *tx_in_sign(tx_in_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH],
 		unspent = llist_get_node_at(all_unspent, i);
 		if (memcmp(unspent->out.hash, in->tx_out_hash, SHA256_DIGEST_LENGTH) == 0)
 		{
-			memcpy(pub_of_reciver, unspent->out.pub, EC_PUB_LEN);
 			break;
 		}
 		i++;
@@ -38,7 +37,7 @@ sig_t *tx_in_sign(tx_in_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH],
 			return (NULL);
 		}
 	}
-	if (memcmp(pub_of_sender, pub_of_reciver, EC_PUB_LEN) != 0)
+	if (memcmp(pub_of_sender, unspent->out.pub, EC_PUB_LEN) != 0)
 		return (NULL);
 	if (!ec_sign(sender, tx_id, SHA256_DIGEST_LENGTH, &in->sig))
 		return (NULL);
