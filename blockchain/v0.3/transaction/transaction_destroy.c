2598e747a1a5cec827b03101066d3fe5304f9a40
louay66 <louaylouay20@gmail.com>
2023-08-10 22:33:05 +0100

    udate

--- blockchain/v0.3/transaction/transaction_destroy.c
+++ blockchain/v0.3/transaction/transaction_destroy.c
@@ -6,9 +6,13 @@
  */
 void transaction_destroy(transaction_t *transaction)
 {
-	if (!transaction)
+	if (transaction == NULL)
 		return;
-	llist_destroy(transaction->inputs, 1, free);
-	llist_destroy(transaction->outputs, 1, free);
+
+	if (transaction->inputs)
+		llist_destroy(transaction->inputs, 1, NULL);
+	if (transaction->outputs)
+		llist_destroy(transaction->outputs, 1, NULL);
+
 	free(transaction);
 }
