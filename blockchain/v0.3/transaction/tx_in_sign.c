85ccfece64dabac3d5cff790db403e9f508baca4
louay66 <louaylouay20@gmail.com>
2023-08-10 22:58:58 +0100

    test

--- blockchain/v0.3/transaction/tx_in_sign.c
+++ blockchain/v0.3/transaction/tx_in_sign.c
@@ -15,32 +15,22 @@
 sig_t *tx_in_sign(tx_in_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH],
 						EC_KEY const *sender, llist_t *all_unspent)
 {
-	uint8_t pub_of_sender[EC_PUB_LEN];
-	uint8_t pub_of_reciver[EC_PUB_LEN];
-	unspent_tx_out_t *unspent;
-	unsigned int i;
+	uint8_t pub[EC_PUB_LEN];
+	ssize_t i;
+	unspent_tx_out_t *utx;
 
-	if (!ec_to_pub(sender, pub_of_sender))
-		return (NULL);
-
-	i = 0;
-	while (1)
+	for (i = 0; i < llist_size(all_unspent); i++)
 	{
-		unspent = llist_get_node_at(all_unspent, i);
-		if (memcmp(unspent->out.hash, in->tx_out_hash, SHA256_DIGEST_LENGTH) == 0)
-		{
-			memcpy(pub_of_reciver, unspent->out.pub, EC_PUB_LEN);
+		utx = llist_get_node_at(all_unspent, i);
+		if (!memcmp(in->tx_out_hash, utx->out.hash, SHA256_DIGEST_LENGTH))
 			break;
-		}
-		i++;
-		if (!unspent)
-		{
-			return (NULL);
-		}
+		utx = NULL;
 	}
-	if (memcmp(pub_of_sender, pub_of_reciver, EC_PUB_LEN) != 0)
+
+	if (!utx || !ec_to_pub(sender, pub) || memcmp(pub, utx->out.pub, EC_PUB_LEN))
 		return (NULL);
 	if (!ec_sign(sender, tx_id, SHA256_DIGEST_LENGTH, &in->sig))
 		return (NULL);
+
 	return (&in->sig);
 }
