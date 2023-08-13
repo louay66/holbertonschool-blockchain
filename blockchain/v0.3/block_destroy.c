51aab0e141eb59313d6cd29a64783efbcee7d63a
louay66 <louaylouay20@gmail.com>
2023-08-13 12:23:57 +0100

    block_destroy.c

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -6,6 +6,7 @@
  */
 void block_destroy(block_t *block)
 {
-	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
+	if (block->transactions)
+		llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
 	free(block);
 }
