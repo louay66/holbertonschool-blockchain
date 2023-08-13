52fe06c7de0d40f4979d92792655236a48e76667
louay66 <louaylouay20@gmail.com>
2023-08-13 12:16:49 +0100

    fix

--- blockchain/v0.3/transaction/coinbase_create.c
+++ blockchain/v0.3/transaction/coinbase_create.c
@@ -10,34 +10,34 @@
  */
 transaction_t *coinbase_create(EC_KEY const *receiver, uint32_t block_index)
 {
+	transaction_t *out;
+	tx_in_t *i_token;
+	tx_out_t *o_token;
+	uint8_t key_out[EC_PUB_LEN];
 
-	uint8_t pub[EC_PUB_LEN];
-	tx_out_t *tx_out;
-	tx_in_t *tx_in;
-	transaction_t *tx;
-
-	if (!receiver || !block_index || !ec_to_pub(receiver, pub))
+	out = malloc(sizeof(transaction_t));
+	if (out == NULL)
 		return (NULL);
-
-	tx = calloc(1, sizeof(transaction_t));
-	tx_in = calloc(1, sizeof(tx_in_t));
-
-	if (!tx || !tx_in)
+	i_token = malloc(sizeof(tx_in_t));
+	if (i_token == NULL)
 	{
-		return (free(tx), free(tx_in), NULL);
+		free(out);
+		return (NULL);
 	}
-	memset(tx_in, 0, sizeof(*tx_in));
-	memcpy(tx_in->tx_out_hash, &block_index, 4);
-	tx->inputs = llist_create(MT_SUPPORT_FALSE);
-	tx->outputs = llist_create(MT_SUPPORT_FALSE);
-	tx_out = tx_out_create(COINBASE_AMOUNT, pub);
-	llist_add_node(tx->inputs, tx_in, ADD_NODE_REAR);
-	llist_add_node(tx->outputs, tx_out, ADD_NODE_REAR);
-	if (!transaction_hash(tx, tx->id))
-	{
-		return (free(tx), free(tx_in), free(tx_out),
-				  llist_destroy(tx->inputs, 0, NULL),
-				  llist_destroy(tx->outputs, 0, NULL), NULL);
-	}
-	return (tx);
+
+	memset(i_token, 0, sizeof(*i_token));
+	memcpy(i_token->tx_out_hash, &block_index, 4);
+
+	ec_to_pub(receiver, key_out);
+	o_token = tx_out_create(COINBASE_AMOUNT, key_out);
+
+	out->inputs = llist_create(MT_SUPPORT_FALSE);
+	out->outputs = llist_create(MT_SUPPORT_FALSE);
+
+	llist_add_node(out->inputs, i_token, ADD_NODE_REAR);
+	llist_add_node(out->outputs, o_token, ADD_NODE_REAR);
+
+	transaction_hash(out, out->id);
+
+	return (out);
 }
