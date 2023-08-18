9fa9b4ceb1cb6314cdd541a12129e6ab585e5fa2
louay66 <louaylouay20@gmail.com>
2023-08-18 02:27:23 +0100

    i am sure this is true

--- blockchain/v0.3/blockchain_deserialize.c
+++ blockchain/v0.3/blockchain_deserialize.c
@@ -3,13 +3,12 @@
 
 #define MAGIC_HBLK "HBLK"
 #define VERSION_HBLK "0.1"
-#define CHECK_ENDIAN(x) (endianess ? SWAPENDIAN(x) : (void)0)
 
-llist_t *deserialize_unspent(FILE *file, int size, uint8_t endianess);
-llist_t *deserialize_blocks(FILE *file, int size, uint8_t endianess);
-llist_t *deserialize_tx(FILE *file, int size, uint8_t endianess);
+llist_t *deserialize_unspent(FILE *file, int size);
+llist_t *deserialize_blocks(FILE *file, int size);
+llist_t *deserialize_tx(FILE *file, int size);
 llist_t *deserialize_tx_in(FILE *file, int size);
-llist_t *deserialize_tx_out(FILE *file, int size, uint8_t endianess);
+llist_t *deserialize_tx_out(FILE *file, int size);
 
 /**
  * blockchain_deserialize-deserialize a  file format to bloackchain
@@ -28,7 +27,7 @@ blockchain_t *blockchain_deserialize(char const *path)
 	if (!path)
 		return (NULL);
 
-	file = fopen(path, "r");
+	file = fopen(path, "rb");
 	if (!file)
 		return (NULL);
 	fread(&buff, 7, 1, file);
@@ -37,25 +36,20 @@ blockchain_t *blockchain_deserialize(char const *path)
 		fclose(file);
 		return (NULL);
 	}
-	fread(&endianess, 1, 1, file);
-	endianess = endianess != _get_endianness();
-	fread(&size, 4, 1, file);
-	CHECK_ENDIAN(size);
-	fread(&nb_unspent, 4, 1, file);
-	CHECK_ENDIAN(nb_unspent);
-	blockchain = calloc(1, sizeof(*blockchain));
-	blockchain->chain = deserialize_blocks(file, size, endianess);
 
+	fread(&endianess, 1, 1, file);
+	fread(&size, 4, 1, file);
+	fread(&nb_unspent, 4, 1, file);
+	blockchain = calloc(1, sizeof(blockchain_t));
+	blockchain->chain = deserialize_blocks(file, size);
 	if (!blockchain->chain)
 	{
-		blockchain_destroy(blockchain);
 		fclose(file);
 		return (NULL);
 	}
-	blockchain->unspent = deserialize_unspent(file, nb_unspent, endianess);
+	blockchain->unspent = deserialize_unspent(file, nb_unspent);
 	if (!blockchain->unspent)
 	{
-		blockchain_destroy(blockchain);
 		fclose(file);
 		return (NULL);
 	}
@@ -67,60 +61,29 @@ blockchain_t *blockchain_deserialize(char const *path)
  * deserialize_unspent-deserialize a  file format to unspent list
  * @file: file to deserialize blocks from it
  * @size: size of  blocks in blockchain
- * @endianess: if 1 is big endian need to swap byte  else lettle endien
  * Return: if successful return hash otherwise return NULL
  */
-llist_t *deserialize_unspent(FILE *file, int size, uint8_t endianess)
+llist_t *deserialize_unspent(FILE *file, int size)
 {
 	llist_t *list_unspent;
 	unspent_tx_out_t *unspent;
 	int i;
 
-	list_unspent = llist_create(MT_SUPPORT_TRUE);
+	list_unspent = llist_create(MT_SUPPORT_FALSE);
 	if (!list_unspent)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
-		unspent = calloc(1, sizeof(*unspent));
+		unspent = calloc(1, sizeof(unspent_tx_out_t));
 		if (!unspent)
 			return (NULL);
-		if (fread(&unspent->block_hash, SHA256_DIGEST_LENGTH, 1, file) != 1)
-		{
-			free(unspent);
-			llist_destroy(list_unspent, 1, NULL);
-			return (NULL);
-		}
-		if (fread(&unspent->tx_id, SHA256_DIGEST_LENGTH, 1, file) != 1)
-		{
-			free(unspent);
-			llist_destroy(list_unspent, 1, NULL);
-			return (NULL);
-		}
-		if (fread(&unspent->out.amount, 4, 1, file) != 1)
-		{
-			free(unspent);
-			llist_destroy(list_unspent, 1, NULL);
-			return (NULL);
-		}
-		CHECK_ENDIAN(unspent->out.amount);
-		if (fread(&unspent->out.pub, 65, 1, file) != 1)
-		{
-			free(unspent);
-			llist_destroy(list_unspent, 1, NULL);
-			return (NULL);
-		}
-		if (fread(&unspent->out.hash, SHA256_DIGEST_LENGTH, 1, file) != 1)
-		{
-			free(unspent);
-			llist_destroy(list_unspent, 1, NULL);
-			return (NULL);
-		}
-		if (llist_add_node(list_unspent, unspent, ADD_NODE_REAR))
-		{
-			free(unspent);
-			llist_destroy(list_unspent, 1, NULL);
-			return (NULL);
-		}
+		fread(&unspent->block_hash, SHA256_DIGEST_LENGTH, 1, file);
+		fread(&unspent->tx_id, SHA256_DIGEST_LENGTH, 1, file);
+
+		fread(&unspent->out.amount, 4, 1, file);
+		fread(&unspent->out.pub, 65, 1, file);
+		fread(&unspent->out.hash, SHA256_DIGEST_LENGTH, 1, file);
+		llist_add_node(list_unspent, unspent, ADD_NODE_REAR);
 	}
 	return (list_unspent);
 }
@@ -128,61 +91,28 @@ llist_t *deserialize_unspent(FILE *file, int size, uint8_t endianess)
  * deserialize_blocks-deserialize a  file format to blocks
  * @file: file to deserialize blocks from it
  * @size: size of  blocks in blockchain
- * @endianess: if 1 is big endian need to swap byte  else lettle endien
  * Return: if successful return hash otherwise return NULL
  */
-llist_t *deserialize_blocks(FILE *file, int size, uint8_t endianess)
+llist_t *deserialize_blocks(FILE *file, int size)
 {
 	llist_t *chain;
 	block_t *block;
 	int i, nb_tx;
 
-	chain = llist_create(MT_SUPPORT_TRUE);
+	chain = llist_create(MT_SUPPORT_FALSE);
 	if (!chain)
 		return (NULL);
-
 	for (i = 0; i < size; i++)
 	{
-		block = calloc(1, sizeof(*block));
-		if (fread(&block->info, sizeof(block->info), 1, file) != 1)
-		{
-			block_destroy(block);
-			llist_destroy(chain, 1, NULL);
-			return (NULL);
-		}
-		CHECK_ENDIAN(block->info.index);
-		CHECK_ENDIAN(block->info.difficulty);
-		CHECK_ENDIAN(block->info.timestamp);
-		CHECK_ENDIAN(block->info.nonce);
-		if (fread(&block->data.len, 4, 1, file) != 1)
-		{
-			block_destroy(block);
-			llist_destroy(chain, 1, NULL);
-			return (NULL);
-		}
-		CHECK_ENDIAN(block->data.len);
-
-		if (fread(&block->data.buffer, block->data.len, 1, file) != 1)
-		{
-			block_destroy(block);
-			llist_destroy(chain, 1, NULL);
-			return (NULL);
-		}
-
-		if (fread(&block->hash, SHA256_DIGEST_LENGTH, 1, file) != 1)
-		{
-			block_destroy(block);
-			llist_destroy(chain, 1, NULL);
-			return (NULL);
-		}
+		block = calloc(1, sizeof(block_t));
+		fread(&block->info, sizeof(block->info), 1, file);
+		fread(&block->data.len, 4, 1, file);
+		fread(&block->data.buffer, block->data.len, 1, file);
+		fread(&block->hash, SHA256_DIGEST_LENGTH, 1, file);
 		fread(&nb_tx, 4, 1, file);
-		block->transactions = deserialize_tx(file, nb_tx, endianess);
-		if (llist_add_node(chain, block, ADD_NODE_REAR))
-		{
-			block_destroy(block);
-			llist_destroy(chain, 1, NULL);
-			return (NULL);
-		}
+		llist_add_node(block->transactions, deserialize_tx(file, nb_tx),
+							ADD_NODE_REAR);
+		llist_add_node(chain, block, ADD_NODE_REAR);
 	}
 	return (chain);
 }
@@ -190,161 +120,44 @@ llist_t *deserialize_blocks(FILE *file, int size, uint8_t endianess)
  * deserialize_tx-deserialize a  file format to transection of block
  * @file: file to deserialize blocks from it
  * @size: size of  blocks in blockchain
- * @endianess: if 1 is big endian need to swap byte  else lettle endien
  * Return: if successful return hash otherwise return NULL
  */
-llist_t *deserialize_tx(FILE *file, int size, uint8_t endianess)
+llist_t *deserialize_tx(FILE *file, int size)
 {
 
 	llist_t *block_tx;
 	transaction_t *tx;
-	int i, nb_in, nb_out;
+	tx_out_t *out;
+	tx_in_t *input;
+	int j, i, nb_in, nb_out;
 
 	block_tx = llist_create(MT_SUPPORT_FALSE);
-	if (!block_tx)
+	if (size == -1)
+	{
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
-		tx = calloc(1, sizeof(*tx));
+		tx = calloc(1, sizeof(transaction_t));
 		if (!tx)
 			return (NULL);
-		if (fread(&tx->id, SHA256_DIGEST_LENGTH, 1, file) != 1)
-		{
-			transaction_destroy(tx);
-			llist_destroy(block_tx, 1, NULL);
-			return (NULL);
-		}
+		fread(tx, SHA256_DIGEST_LENGTH, 1, file);
 		fread(&nb_in, 4, 1, file);
 		fread(&nb_out, 4, 1, file);
-		tx->inputs = deserialize_tx_in(file, nb_in);
-		if (!tx->inputs)
+		for (j = 0; j < nb_in; j++)
 		{
-			transaction_destroy(tx);
-			llist_destroy(block_tx, 1, NULL);
-			return (NULL);
+			input = calloc(1, sizeof(tx_in_t));
+			fread(input, 169, 1, file);
+			llist_add_node(tx->inputs, input, ADD_NODE_REAR);
 		}
-		tx->outputs = deserialize_tx_out(file, nb_out, endianess);
-		if (!tx->outputs)
+		for (j = 0; j < nb_out; j++)
 		{
-			transaction_destroy(tx);
-			llist_destroy(block_tx, 1, NULL);
-			return (NULL);
+			out = calloc(1, sizeof(tx_out_t));
+			fread(out, 101, 1, file);
+
+			llist_add_node(tx->outputs, out, ADD_NODE_REAR);
 		}
-		if (llist_add_node(block_tx, tx, ADD_NODE_REAR))
-		{
-			transaction_destroy(tx);
-			llist_destroy(block_tx, 1, NULL);
-			return (NULL);
-		}
+		llist_add_node(block_tx, tx, ADD_NODE_REAR);
 	}
 	return (block_tx);
-}
-/**
- * deserialize_tx_in-deserialize a  file format to input of transection
- * @file: file to deserialize blocks from it
- * @size: size of  blocks in blockchain
- * Return: if successful return hash otherwise return NULL
- */
-llist_t *deserialize_tx_in(FILE *file, int size)
-{
-	llist_t *inputs;
-	tx_in_t *input;
-	int i;
-
-	inputs = llist_create(MT_SUPPORT_FALSE);
-	if (!inputs)
-		return (NULL);
-	for (i = 0; i < size; i++)
-	{
-		input = calloc(1, sizeof(*input));
-		if (!input)
-			return (NULL);
-		if (fread(&input->block_hash, SHA256_DIGEST_LENGTH, 1, file) != 1)
-		{
-			free(input);
-			llist_destroy(inputs, 1, NULL);
-			return (NULL);
-		}
-		if (fread(&input->tx_id, SHA256_DIGEST_LENGTH, 1, file) != 1)
-		{
-			free(input);
-			llist_destroy(inputs, 1, NULL);
-			return (NULL);
-		}
-		if (fread(&input->tx_out_hash, SHA256_DIGEST_LENGTH, 1, file) != 1)
-		{
-			free(input);
-			llist_destroy(inputs, 1, NULL);
-			return (NULL);
-		}
-		if (fread(&input->sig.sig, 72, 1, file) != 1)
-		{
-			free(input);
-			llist_destroy(inputs, 1, NULL);
-			return (NULL);
-		}
-		if (fread(&input->sig.len, 1, 1, file) != 1)
-		{
-			free(input);
-			llist_destroy(inputs, 1, NULL);
-			return (NULL);
-		}
-		if (llist_add_node(inputs, input, ADD_NODE_REAR))
-		{
-			free(input);
-			llist_destroy(inputs, 1, NULL);
-			return (NULL);
-		}
-	}
-	return (inputs);
-}
-
-/**
- * deserialize_tx_out-deserialize a  file format to output of transection
- * @file: file to deserialize blocks from it
- * @size: size of  blocks in blockchain
- * @endianess: if 1 is big endian need to swap byte  else lettle endien
- * Return: if successful return hash otherwise return NULL
- */
-llist_t *deserialize_tx_out(FILE *file, int size, uint8_t endianess)
-{
-	llist_t *outputs;
-	tx_out_t *out;
-	int i;
-
-	outputs = llist_create(MT_SUPPORT_FALSE);
-	if (!outputs)
-		return (NULL);
-	for (i = 0; i < size; i++)
-	{
-		out = calloc(1, sizeof(*out));
-		if (!out)
-			return (NULL);
-		if (fread(&out->amount, 4, 1, file) != 1)
-		{
-			free(out);
-			llist_destroy(outputs, 1, NULL);
-			return (NULL);
-		}
-		CHECK_ENDIAN(out->amount);
-		if (fread(&out->pub, 65, 1, file) != 1)
-		{
-			free(out);
-			llist_destroy(outputs, 1, NULL);
-			return (NULL);
-		}
-		if (fread(&out->hash, SHA256_DIGEST_LENGTH, 1, file) != 1)
-		{
-			free(out);
-			llist_destroy(outputs, 1, NULL);
-			return (NULL);
-		}
-		if (llist_add_node(outputs, out, ADD_NODE_REAR))
-		{
-			free(out);
-			llist_destroy(outputs, 1, NULL);
-			return (NULL);
-		}
-	}
-	return (outputs);
 }
